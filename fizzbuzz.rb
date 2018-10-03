7f31a0037f65a374ac6392440b6a2d749486fde7
Yu Li <yli0607x@gmail.com>
2018-10-03 20:48:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(number)
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
-  elsif number % 15 == 0
+  elsif number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
 end
 end
